58405c501ea93a4f6b71a3ffcd6417e4f48fc23b
Monick Marinho <monickmarinhoc@gmail.com>
2024-02-27 19:30:00 -0300

    uptade arq

---
+++ src/pages/FormCadastro.jsx
@@ -0,0 +1,38 @@
+import "../style.form.scss";
+import BtnSubmit from "../form/BtnSubmit";
+import BtnSocial from "../components/buttonsocial/BtnSocial";
+import Input from "../form/Input";
+import InputEmail from "../components/InputEmail";
+import InputSenha from "../components/InputSenha";
+import { FiUser } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+export default function FormCadastro() {
+  return (
+    <>
+      <form>
+        <h1>Cadastro</h1>
+        <div className="input-container">
+          <Input
+            type="text"
+            placeholder="3 caracteres ou mais"
+            title="Nome"
+            name="nome"
+            id="nome"
+            icon={<FiUser className="icon" />}
+            // value={e.target.name}
+          />
+          <InputEmail />
+          <InputSenha />
+        </div>
+        <BtnSubmit textbtn="Cadastrar" />
+        <BtnSocial />
+        <div>
+          <span>
+            Já tem conta? Faça o <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </form>
+    </>
+  );
+}
---
+++ src/pages/FormLogin.jsx
@@ -0,0 +1,29 @@
+import "../style.form.scss";
+import BtnSubmit from "../form/BtnSubmit";
+import BtnSocial from "../components/buttonsocial/BtnSocial";
+import InputEmail from "../components/InputEmail";
+import InputSenha from "../components/InputSenha";
+import { Link } from "react-router-dom";
+
+export default function FormLogin() {
+  return (
+    <>
+      <form>
+        <h1>Login</h1>
+        <div className="input-container">
+          <InputEmail />
+          <InputSenha />
+        </div>
+
+        <BtnSubmit textbtn="Logar" />
+        <BtnSocial />
+
+        <div>
+          <span>
+            Ainda não tem conta? <Link to="/cadastro">Cadastra-se</Link>
+          </span>
+        </div>
+      </form>
+    </>
+  );
+}
